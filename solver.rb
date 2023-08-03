b101cbb423bd25fb7769569b8e37abde53297b77
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-03 14:14:24 -0300

    Implement code for passing factorial tests

--- solver.rb
+++ solver.rb
@@ -1,3 +1,11 @@
 class Solver
+  def factorial(num)
+    raise ArgumentError, 'No factorial for negative numbers' if num.negative?
 
+    if num.zero? || num == 1
+      1
+    elsif num > 1
+      num * factorial(num - 1)
+    end
+  end
 end
