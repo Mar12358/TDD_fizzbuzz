94b7939b21cfe568d571fc0032ace8788f73d004
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-03 14:13:58 -0300

    Add tests for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,21 @@
 require_relative '../solver'
 
 RSpec.describe Solver do
-  
+  solve = Solver.new
+  describe '#factorial' do
+    it 'returns 1 when N = 0 or N = 1' do
+      expect(solve.factorial(0)).to eql(1)
+      expect(solve.factorial(1)).to eql(1)
+    end
+
+    it 'returns !n for n > 1' do
+      expect(solve.factorial(3)).to eql(6)
+      expect(solve.factorial(4)).to eql(24)
+      expect(solve.factorial(5)).to eql(120)
+    end
+
+    it 'returns error exception' do
+      expect { solve.factorial(-1) }.to raise_error(ArgumentError, 'No factorial for negative numbers')
+    end
+  end
 end
